24a0984a8cd552657964f2691038ce99423f6c35
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2024-12-06 02:15:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const background = document.getElementById('background');
 
-// Random position and animation for XHOTEL text
 function createText() {
     const text = document.createElement('div');
     text.classList.add('text');
@@ -9,11 +8,9 @@ function createText() {
     text.style.left = Math.random() * 100 + '%';
     background.appendChild(text);
 
-    // Remove the text after animation ends
     setTimeout(() => {
         text.remove();
     }, 4000);
 }
 
-// Generate text every 500ms
 setInterval(createText, 500);
